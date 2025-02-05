892d28557ea6c4f15bc7eb49bf9aa23573f359d1
Petterson Grande <grande.petterson@gmail.com>
2025-02-05 17:58:51 -0300

    Add sentences functionality and delete individual tasks functionality.

--- app.js
+++ app.js
@@ -1,24 +1,66 @@
 // SELECTORS
-const form = document.getElementById('mainForm');
-const imgLink = document.getElementById('imgLink');
-const topTxt = document.getElementById('topTxt');
-const btmTxt = document.getElementById('btmTxt');
-const memeContainer = document.getElementById('memesContainer');
+const form          = document.getElementById('mainForm');
+const imgLink       = document.getElementById('imgLink');
+const topTxt        = document.getElementById('topTxt');
+const btmTxt        = document.getElementById('btmTxt');
+const memeDisplay   = document.getElementById('memesContainer');
+const deleteButton  = document.getElementById('dltBtm');
+const LS_NAME       = "allImgObjs";
 
-const allObjs = [];
+
+// Array of Objects
+const allObjs = retrieveFromLocalStorage(LS_NAME);
+
+renderPage(allObjs, memeDisplay);
 
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
+
+    // USER INPUTS VALUE
+    const imageLink    = imgLink.value;
+    const topString    = topTxt.value;
+    const btmString    = btmTxt.value;
     
-    const newObj = objectFactory(imgLink.value, topTxt.value, btmTxt.value);
-    const newImg = imgCreator(newObj.img);
-    memeContainer.appendChild(newImg);
+    // Create new obj
+    const newObj = objectFactory(imageLink, topString, btmString);    
+    
+    // Storing new object
+    allObjs.push(newObj);
+    saveToLocalStorage(LS_NAME,allObjs);
+    
+    // Create a new Image, append it to memepage and refresh page with allmemes.
+    
+    renderPage(allObjs, memeDisplay);
+
+    // Clearing Input Fields
+    imgLink.value = '';
+    topTxt.value  = '';
+    btmTxt.value  = '';
 
 });
 
-// This function creates an object extracting data from an user input.
-// Parameter: image src, string1, string2;
+// REMOVE ALL MEMES EVENT
+deleteButton.addEventListener('click', (e)=>{
+    localStorage.clear();
+    location.reload();
+});
+
+
+// LOOP AND DISPLAY IMGS
+// Parameter 1: the array where the objects are stored
+// Parameter 2: the area where the images must be displayed
+function renderPage(arrayOfObjects, displayArea){
+    displayArea.innerHTML = '';
+
+    for(let links of arrayOfObjects){
+        displayArea.append(imgCreator(links.img));
+    };
+
+}
+
+// CREATE OBJECT FROM USERINPUT
+// Parameter 1, 2, 3: image src, string1, string2;
 // return: a new object with three keys to be accessed
 function objectFactory(imgSrc, txt1, txt2){
     const newObj = {
@@ -28,11 +70,11 @@ function objectFactory(imgSrc, txt1, txt2){
 
     };
 
-    return newObj
+    return newObj;
 };
 
-// Extracting data from an object to create a new DIV & IMG element.
-// Parameter: object.img >> imgSrc
+// CREATE IMG AND DIV from OBJECT
+// Parameter 1: object.img >> imgSrc
 // Return: new img element.
 function imgCreator(imgSrc){
     const newDiv = document.createElement('DIV');
@@ -44,4 +86,21 @@ function imgCreator(imgSrc){
 
 };
 
-// APPEND NEW IMAGE TO IMAGE CONTAINER
+
+//ADD TO LOCAL STORAGE
+//Parameter 1: storage name and the objecs to be saved
+//Parameter 2: Objets to be stored in local storage
+//Return: nothing.
+function saveToLocalStorage(storageName, objectToBeSaved) {
+    localStorage.setItem(storageName, JSON.stringify(objectToBeSaved));
+
+    return;
+};
+
+//RETRIEVE FROM LOCAL STORAGE
+//Parameter:
+//Return: objects stored in session storage;
+function retrieveFromLocalStorage(storageName){
+    return JSON.parse(localStorage.getItem(storageName)) || [];
+
+};
