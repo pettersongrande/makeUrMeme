88e6671f3ae53452449750e0c4ac0db0914b06a4
Petterson Grande <grande.petterson@gmail.com>
2025-02-07 19:58:33 -0300

    Add captions functionality to app.

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const memeDisplay   = document.getElementById('memesContainer');
 const deleteButton  = document.getElementById('dltBtm');
 const LS_NAME       = "allImgObjs";
 
-
 // Array of Objects
 const allObjs = retrieveFromLocalStorage(LS_NAME);
 
+// alert('Hello there, welcome to  Make Your Meme. INSTRUCTIONS choose a gif or image of your choice copy and paste its link in the first field. Next, add captions. You can add top and bottom, but If you did not enjoy your new creation you can double click in the image and it will disappear. The trash button on the right will delete all your memes, so be careful or not...');
+
 renderPage(allObjs, memeDisplay);
 
-
+// EVENTS
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-
+    
     // USER INPUTS VALUE
     const imageLink    = imgLink.value;
     const topString    = topTxt.value;
@@ -25,12 +26,21 @@ form.addEventListener('submit',(e)=>{
     // Create new obj
     const newObj = objectFactory(imageLink, topString, btmString);    
     
+    // Setting the IndexNumber to the current array length
+    const indexNumber  = allObjs.length;
+
     // Storing new object
     allObjs.push(newObj);
+    
+    // Create new IMAGE
+    const newImg = imgCreator(allObjs[indexNumber].img, indexNumber);
+
+     // Saving to Local Storage
     saveToLocalStorage(LS_NAME,allObjs);
     
-    // Create a new Image, append it to memepage and refresh page with allmemes.
-    
+    memeDisplay.append(newImg);
+
+    // Render the whole page displaying all images stored in allObjs/localStorage
     renderPage(allObjs, memeDisplay);
 
     // Clearing Input Fields
@@ -39,6 +49,29 @@ form.addEventListener('submit',(e)=>{
     btmTxt.value  = '';
 
 });
+
+// REMOVE EACH MEME EVENT
+memeDisplay.addEventListener('dblclick', (e)=>{
+    e.preventDefault();
+    const memeIndex = e.target.dataset.indxNum;
+
+    if(e.target.tagName === 'IMG'){
+        deleteMeme(memeIndex, allObjs);
+        renderPage(allObjs, memeDisplay);
+    }
+    else {
+        return;
+    }
+    
+});
+
+// REMOVE EACH MEME FUNCTION
+// Parameter 1: index number
+// Parameter 2: arrayOfObjects
+function deleteMeme(idxNum, arrayOfObjects){
+    arrayOfObjects.splice(idxNum, 1);
+    saveToLocalStorage(LS_NAME, arrayOfObjects);
+};
 
 // REMOVE ALL MEMES EVENT
 deleteButton.addEventListener('click', (e)=>{
@@ -50,14 +83,15 @@ deleteButton.addEventListener('click', (e)=>{
 // LOOP AND DISPLAY IMGS
 // Parameter 1: the array where the objects are stored
 // Parameter 2: the area where the images must be displayed
+// Return: nothing;
 function renderPage(arrayOfObjects, displayArea){
     displayArea.innerHTML = '';
 
-    for(let links of arrayOfObjects){
-        displayArea.append(imgCreator(links.img));
+    for(let i = 0; i < arrayOfObjects.length; i++){
+        displayArea.append(imgCreator(arrayOfObjects[i].img, i));
     };
 
-}
+};
 
 // CREATE OBJECT FROM USERINPUT
 // Parameter 1, 2, 3: image src, string1, string2;
@@ -76,9 +110,10 @@ function objectFactory(imgSrc, txt1, txt2){
 // CREATE IMG AND DIV from OBJECT
 // Parameter 1: object.img >> imgSrc
 // Return: new img element.
-function imgCreator(imgSrc){
+function imgCreator(imgSrc, indexNumber){
     const newDiv = document.createElement('DIV');
     const newImg = document.createElement('IMG');
+    newImg.dataset.indxNum = indexNumber;
     newImg.setAttribute("src", imgSrc);
     newDiv.innerHTML = newImg;  
 
@@ -104,3 +139,18 @@ function retrieveFromLocalStorage(storageName){
     return JSON.parse(localStorage.getItem(storageName)) || [];
 
 };
+
+// AREA FOR TESTING ---------------------------------------------------------
+function testingLoop(arrayOfObjects){
+
+    for(let links of arrayOfObjects){
+        console.log(links.img)
+    }
+
+    // for (let i = 0; i < arrayOfObjects.length; i++){
+    //     console.log(arrayOfObjects[i].img);
+    // };
+};
+// testingLoop(allObjs)
+
+
